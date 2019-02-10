d3b7ed66c976f3f5e0a5224441f83c7d1e50db17
Regs <regs@usjr.edu.ph>
2019-02-10 22:28:33 +0800

    update repo

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/m/api/register',
+        '/m/api/login',
+        '/m/api/report/send'
     ];
 }
